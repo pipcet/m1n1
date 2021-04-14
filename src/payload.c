c8fbd2fb3565a1838ececd018000021928562762
Pip Cet <pipcet@gmail.com>
2021-04-14 18:10:45 +0000

    support macho payloads

--- src/payload.c
+++ src/payload.c
@@ -7,6 +7,7 @@
 #include "smp.h"
 #include "utils.h"
 
+#include "malloc.h"
 #include "libfdt/libfdt.h"
 #include "minilzlib/minlzma.h"
 #include "tinf/tinf.h"
@@ -19,9 +20,12 @@ const u8 xz_magic[] = {0xfd, '7', 'z', 'X', 'Z', 0x00};
 const u8 fdt_magic[] = {0xd0, 0x0d, 0xfe, 0xed};
 const u8 kernel_magic[] = {'A', 'R', 'M', 0x64};   // at 0x38
 const u8 cpio_magic[] = {'0', '7', '0', '7', '0'}; // '1' or '2' next
+const u8 macho_magic[] = { 0xcf, 0xfa, 0xed, 0xfe };
 const u8 empty[] = {0, 0, 0, 0};
 
 struct kernel_header *kernel = NULL;
+void *macho_start_pc = NULL;
+void *macho_start_secondary_pc = NULL;
 void *fdt = NULL;
 
 static void *load_one_payload(void *start, size_t size);
@@ -135,6 +139,90 @@ static void *load_kernel(void *p, size_t size)
     }
 }
 
+struct macho_command {
+    u32 type;
+    u32 size;
+    union {
+	struct {
+	    u32 thread_type;
+	    u32 length;
+	    u64 regs[32];
+	    u64 pc;
+	    u64 regs2[1];
+	} unix_thread;
+	struct {
+	    char segname[16];
+	    u64 vmaddr;
+	    u64 vmsize;
+	    u64 fileoff;
+	    u64 filesize;
+	    u64 unused2[2];
+	} segment_64;
+    } u;
+};
+
+static void *load_macho(void *start, size_t size)
+{
+    UNUSED(size);
+    struct macho_command *last_command = start + 32 + ((u32 *)start)[5];
+    struct macho_command *command = start + 32;
+    u64 pc = 0;
+    u64 vmbase = 0;
+    u64 vmtotalsize = 0;
+    while (command < last_command) {
+	switch (command->type) {
+	case 0x05:
+	    pc = command->u.unix_thread.pc;
+	    break;
+	case 0x19: {
+	    u64 vmaddr = command->u.segment_64.vmaddr;
+	    u64 vmsize = command->u.segment_64.vmsize;
+
+	    if (vmbase == 0)
+		vmbase = vmaddr;
+	    if (vmsize + vmbase - vmaddr > vmtotalsize)
+		vmtotalsize = vmsize + vmaddr - vmbase;
+	    break;
+	}
+	}
+	command = (void *)command + command->size;
+    }
+    void *dest = memalign(0x10000, vmtotalsize);
+    memset(dest, 0, vmtotalsize);
+    command = start + 32;
+    void *virtpc = NULL;
+    while (command < last_command) {
+	switch (command->type) {
+	case 0x19: {
+	    if (vmbase == 0)
+		vmbase = command->u.segment_64.vmaddr;
+	    u64 vmaddr = command->u.segment_64.vmaddr;
+	    u64 vmsize = command->u.segment_64.vmsize;
+	    u64 pcoff = pc - vmaddr;
+	    u64 fileoff = command->u.segment_64.fileoff;
+	    u64 filesize = command->u.segment_64.filesize;
+
+	    printf("pcoff %p vmaddr %p vmbase %p vmsize %p file %p %p\n", pcoff,
+		   vmaddr, vmbase, vmsize, fileoff, filesize);
+	    memcpy(dest + vmaddr - vmbase, start + fileoff, filesize);
+	    if (pcoff < vmsize) {
+
+		if (pcoff < filesize) {
+		    virtpc = dest + vmaddr - vmbase + pcoff;
+		    macho_start_secondary_pc = dest + vmaddr - vmbase;
+		}
+	    }
+	}
+	}
+	command = (void *)command + command->size;
+    }
+
+    macho_start_pc = virtpc;
+    printf("pcs %p %p\n", macho_start_pc, macho_start_secondary_pc);
+
+    return NULL;
+}
+
 static void *load_one_payload(void *start, size_t size)
 {
     u8 *p = start;
@@ -157,6 +245,9 @@ static void *load_one_payload(void *start, size_t size)
     } else if (!memcmp(p + 0x38, kernel_magic, sizeof kernel_magic)) {
         printf("Found a kernel at %p\n", p);
         return load_kernel(p, size);
+    } else if (!memcmp(p, macho_magic, sizeof macho_magic)) {
+        printf("Found a Mach-O image at %p\n", p);
+        return load_macho(p, size);
     } else if (!memcmp(p, empty, sizeof empty)) {
         printf("No more payloads at %p\n", p);
         return NULL;
@@ -166,12 +257,33 @@ static void *load_one_payload(void *start, size_t size)
     }
 }
 
+extern u64 boot_args_addr;
+extern void mmu_shutdown(void);
+
+int macho_boot(void *entry, void *secondary_entry)
+{
+    smp_start_secondaries();
+    for (int i = 0; i < 8; i++)
+	smp_call4(i, secondary_entry, 0, 0, 0, 0);
+    mmu_shutdown();
+    printf("calling macho at %p / %p\n",
+	   entry, secondary_entry);
+
+    ((void (*)(u64, u64, u64, u64))entry)(boot_args_addr, 0, 0, 0);
+
+    panic("macho call returned\n");
+}
+
 void payload_run(void)
 {
     void *p = _payload_start;
 
     while (p)
         p = load_one_payload(p, 0);
+
+    if (macho_start_pc) {
+	macho_boot(macho_start_pc, macho_start_secondary_pc);
+    }
 
     if (kernel && fdt) {
         smp_start_secondaries();
