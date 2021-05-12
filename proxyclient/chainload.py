6968b6fbc54a2710c6d156301a0023005588d927
Pip Cet <pipcet@gmail.com>
2021-05-12 13:44:59 +0000

    proxyclient/chainload.py: update for linux+macos hack

--- proxyclient/chainload.py
+++ proxyclient/chainload.py
@@ -6,6 +6,8 @@ parser = argparse.ArgumentParser(description='Mach-O loader for m1n1')
 parser.add_argument('payload', type=pathlib.Path)
 parser.add_argument('-s', '--sepfw', action="store_true")
 parser.add_argument('-c', '--call', action="store_true")
+parser.add_argument('-d', '--debug', action="store_true")
+parser.add_argument('-i', '--high', action="store_true")
 args = parser.parse_args()
 
 from setup import *
@@ -13,13 +15,17 @@ from tgtypes import BootArgs
 from macho import MachO
 import asm
 
-if args.sepfw:
+if args.debug:
     p.smp_start_secondaries()
 macho = MachO(args.payload.read_bytes())
 
 image = macho.prepare_image()
 
 new_base = u.base
+if args.high:
+    new_base = 0xa40000000
+    u.get_adt()
+    u.ba.devtree = u.ba.virt_base - u.ba.phys_base + 0xa30000000
 
 entry = macho.entry
 entry -= macho.vmin
@@ -41,8 +47,9 @@ image_size += 0x4000
 
 print(f"Total region size: 0x{image_size:x} bytes")
 image_addr = u.malloc(image_size + 1024 * 1024)
+image_addr = u.malloc(image_size + 1024 * 1024)
 
-print(f"Loading kernel image (0x{len(image):x} bytes)...")
+print(f"Loading kernel image (0x{len(image):x} bytes) to 0x{image_addr:x}...")
 u.compressed_writemem(image_addr, image, True)
 p.dc_cvau(image_addr, len(image))
 
@@ -50,28 +57,34 @@ if args.sepfw:
     print(f"Copying SEPFW (0x{sepfw_length:x} bytes)...")
     p.memcpy8(image_addr + sepfw_off, sepfw_start, sepfw_length)
     print(f"Adjusting addresses in ADT...")
-    u.adt["chosen"]["memory-map"].SEPFW = (new_base + sepfw_off, sepfw_length)
+    u.adt["chosen"]["memory-map"].SEPFW = ((new_base + sepfw_off), sepfw_length)
     u.push_adt()
     print(f"Copying TrustCache (0x{tc_length:x} bytes) from 0x{tc_start:x}...")
     p.memcpy8(image_addr + tc_off, tc_start, tc_length)
     print(f"Adjusting addresses in ADT...")
-    u.adt["chosen"]["memory-map"].TrustCache = (new_base + tc_off, tc_length)
+    u.adt["chosen"]["memory-map"].TrustCache = ((new_base + tc_off), tc_length)
 
+print(u.ba)
 tba = u.ba.copy()
-del u.adt["cpus"]["cpu1"]
-del u.adt["cpus"]["cpu2"]
-del u.adt["cpus"]["cpu3"]
-del u.adt["cpus"]["cpu4"]
-del u.adt["cpus"]["cpu5"]
-del u.adt["cpus"]["cpu6"]
-u.adt["cpus"]["cpu7"].state = "busy"
-u.adt["cpus"].max_cpus = 1
-u.adt["chosen"]["memory-map"].BootArgs = (new_base + bootargs_off, 0x4000)
-u.adt["chosen"]["memory-map"].DeviceTree = (u.ba.devtree, u.ba.devtree_size)
-u.adt["chosen"].dram_size = 0x100000000
+if not args.high:
+    u.adt["chosen"]["memory-map"].BootArgs = ((new_base + bootargs_off), 0x4000)
+    u.adt["chosen"]["memory-map"].DeviceTree = (u.ba.devtree - u.ba.virt_base + u.ba.phys_base, u.ba.devtree_size)
+    u.push_adt()
+    u.push_adt()
+    u.adt["chosen"].dram_size = 0x100000000
+    u.push_adt()
+    del u.adt["cpus"]["cpu1"]
+    del u.adt["cpus"]["cpu2"]
+    del u.adt["cpus"]["cpu3"]
+    del u.adt["cpus"]["cpu4"]
+    del u.adt["cpus"]["cpu5"]
+    del u.adt["cpus"]["cpu6"]
+    u.adt["cpus"]["cpu7"].state = "busy"
+    u.adt["cpus"].max_cpus = 1
+    u.adt["chosen"]["memory-map"].MemoryMapReserved_0 = (0xa00000000, 0x00000000)
+    u.push_adt()
 tba.cmdline = "-v cpus=1"
-u.push_adt()
-print(f"Setting up bootargs...")
+print(f"Setting up bootargs at {image_addr:x} + {bootargs_off:x}, ADT at {tba.devtree}, new_base at {new_base:x}, image_size {image_size:x}")
 
 if args.sepfw:
     tba.top_of_kernel_data = new_base + image_size
@@ -80,12 +93,13 @@ else:
     tba.top_of_kernel_data = max(tba.top_of_kernel_data, new_base + image_size)
 tba.top_of_kernel_data = new_base + image_size
 # tba.mem_size_actual = 0x200000000
-tba.mem_size_actual = 0x200000000
-tba.mem_size = 0x300000000
+tba.mem_size_actual = 0x000000
+tba.mem_size = 0x100000000
+print(tba)
 
 iface.writemem(image_addr + bootargs_off, BootArgs.build(tba))
 
-print(f"Copying stub...")
+print(f"Copying stub... ({image_size:x} bytes to {new_base:x} from {image_addr:x})")
 
 stub = asm.ARMAsm(f"""
 1:
@@ -100,6 +114,21 @@ stub = asm.ARMAsm(f"""
         ldr x1, ={entry}
         br x1
 """, image_addr + image_size)
+if args.high:
+    stub = asm.ARMAsm(f"""
+    1:
+            ldr x4, [x1], #8
+            str x4, [x2]
+            dc cvau, x2
+            ic ivau, x2
+            add x2, x2, #8
+            sub x3, x3, #8
+            cbnz x3, 1b
+
+            ldr x1, ={entry}
+            br x1
+    """, image_addr + image_size)
+
 
 iface.writemem(stub.addr, stub.data)
 p.dc_cvau(stub.addr, stub.len)
@@ -121,24 +150,15 @@ iface.writemem(0xa00020000, f.read(1024 * 1024))
 # p.smp_call(4, 0xa00000000)
 # p.smp_call(5, 0xa00000000)
 # p.smp_call(6, 0xa00000000)
-f = open("m1lli/asm-snippets/fadescreen.c.S.elf.bin", "rb")
-iface.writemem(0xb00000000, f.read(1024 * 1024))
-f = open("build/m1n1.macho", "rb")
-iface.writemem(0xb20000000, f.read(1024 * 1024))
-p.write64(0xb20000000, image_addr + bootargs_off)
+if args.debug and not args.high:
+    f = open("m1lli/asm-snippets/fadescreen.c.S.elf.bin", "rb")
+    iface.writemem(0xab0000000, f.read())
+    f = open("build/m1n1.macho", "rb")
+    iface.writemem(0xac0000000, f.read())
+    p.write64(0xac0000000, new_base + bootargs_off)
 
-print(p.read64(0xb20010800))
-# p.smp_call_sync(7, 0xb20010800, image_addr + bootargs_off)
-if args.sepfw:
-    time.sleep(1)
-    # p.smp_call(1, 0xa00000000)
-    # p.smp_call(2, 0xa00000000)
-    # p.smp_call(3, 0xa00000000)
-    # p.smp_call(4, 0xa00000000)
-    # p.smp_call(5, 0xa00000000)
-    # p.smp_call(6, 0xa00000000)
-    p.smp_call(7, 0xb00000000)
-    time.sleep(1)
+if args.debug:
+    p.smp_call(7, 0xab0000000)
 
 if args.call:
     print(f"Shutting down MMU...")
@@ -149,7 +169,7 @@ if args.call:
     print(f"Jumping to stub at 0x{stub.addr:x}")
     p.call(stub.addr, new_base + bootargs_off, image_addr, new_base, image_size, reboot=True)
 else:
-    print(f"Rebooting into stub at 0x{stub.addr:x}")
+    print(f"Rebooting into stub at 0x{stub.addr:x}, new_base {new_base:x}+{bootargs_off:x} image_addr {image_addr:x}")
     p.reboot(stub.addr, new_base + bootargs_off, image_addr, new_base, image_size)
 
 time.sleep(1)
