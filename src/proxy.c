e097ac21d2a68c58f90ac3046acdf65421b7578b
Pip Cet <pipcet@gmail.com>
2021-05-30 09:54:04 +0000

    src/proxy.c: fix compilation

--- src/proxy.c
+++ src/proxy.c
@@ -461,12 +461,6 @@ int proxy_process(ProxyRequest *request, ProxyReply *reply)
         case P_FB_DISPLAY_LOGO:
             fb_display_logo();
             break;
-        case P_FB_RESTORE_LOGO:
-            fb_restore_logo();
-            break;
-        case P_FB_IMPROVE_LOGO:
-            fb_improve_logo();
-            break;
 
         default:
             reply->status = S_BADCMD;
